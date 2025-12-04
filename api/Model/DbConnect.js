af6517738607cab9cdb39e77e780f246d7e4912c
Ansil <ansilpa211@gmail.com>
2025-12-04 22:10:20 +0530

    change

--- api/Model/DbConnect.js
+++ api/Model/DbConnect.js
@@ -1,12 +1,25 @@
-const mongoose = require('mongoose')
-const dbConnect = async() =>{
-    try{
-    await mongoose.connect(process.env.mongourl)
-    console.log("Database Connection Successful")
-    }
-    catch(err){
-    console.log("Database Connection Failed")
-    }
+// const mongoose = require('mongoose')
+// const dbConnect = async() =>{
+//     try{
+//     await mongoose.connect(process.env.mongourl)
+//     console.log("Database Connection Successful")
+//     }
+//     catch(err){
+//     console.log("Database Connection Failed")
+//     }
+// }
+
+// module.exports = dbConnect
+
+const mongoose = require("mongoose")
+
+async function dbConnect() {
+  try {
+    await mongoose.connect(process.env.mongour)
+    console.log("MongoDB Connected ✔️")
+  } catch(err) {
+    console.log("MongoDB Error ❌", err)
+  }
 }
 
 module.exports = dbConnect
